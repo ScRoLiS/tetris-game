a4ce215bb8650d065eae9cb1e6cc0113b450c201
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-05-12 17:26:48 +0600

    add: render and append

--- src/game/Field.ts
+++ src/game/Field.ts
@@ -1,3 +1,5 @@
+import { Config } from './Config';
+import { Figure } from './Figure';
 import { FigurePart } from './FigurePart';
 
 export class Field {
@@ -27,5 +29,25 @@ export class Field {
 
   constructor() {
 
+  }
+
+  render(ctx: CanvasRenderingContext2D) {
+    this.field.forEach((row, i) => {
+      row.forEach((item, j) => {
+        if (typeof item !== 'number') {
+          item.render(j, i, ctx)
+        }
+      })
+    })
+  }
+
+  append(figure: Figure) {
+    figure.getTemplate().forEach((row, i) => {
+      row.forEach((item, j) => {
+        if (typeof item !== 'number') {
+          this.field[figure.getY() + i][figure.getX() + j] = item
+        }
+      })
+    })
   }
 }
