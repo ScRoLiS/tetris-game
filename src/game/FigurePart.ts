864773dea5f6603a3f60aca4ba816f4a00bcd699
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-05-17 13:05:21 +0600

    add: drawLine method

--- src/game/FigurePart.ts
+++ src/game/FigurePart.ts
@@ -9,7 +9,12 @@ export class FigurePart {
   }
 
   render(x: number, y: number, ctx: CanvasRenderingContext2D) {
-    const lineWidth = 2.5
+    const lineWidth = 3.5
+    const leftX = x * Config.partSize
+    const rightX = leftX + Config.partSize
+    const topY = y * Config.partSize
+    const bottomY = topY + Config.partSize
+    const lineOffset = lineWidth / 2
 
     ctx.fillStyle = this.color
     ctx.fillRect(x * Config.partSize, y * Config.partSize, Config.partSize, Config.partSize)
@@ -17,19 +22,20 @@ export class FigurePart {
     ctx.beginPath()
     ctx.lineWidth = lineWidth
     ctx.strokeStyle = '#00000022'
-    ctx.moveTo(x * Config.partSize + Config.partSize, -lineWidth / 2 + y * Config.partSize + Config.partSize)
-    ctx.lineTo(x * Config.partSize, -lineWidth / 2 + y * Config.partSize + Config.partSize)
-    ctx.moveTo(-lineWidth / 2 + x * Config.partSize + Config.partSize, + y * Config.partSize + Config.partSize)
-    ctx.lineTo(-lineWidth / 2 + x * Config.partSize + Config.partSize, + y * Config.partSize)
+    this.drawLine(rightX - lineOffset, topY, rightX - lineOffset, bottomY, ctx)
+    this.drawLine(leftX, bottomY - lineOffset, rightX, bottomY - lineOffset, ctx)
     ctx.stroke()
 
     ctx.beginPath()
     ctx.lineWidth = lineWidth
     ctx.strokeStyle = '#ffffffbb'
-    ctx.moveTo(lineWidth / 2 + x * Config.partSize, lineWidth / 2 + y * Config.partSize)
-    ctx.lineTo(x * Config.partSize + Config.partSize, lineWidth / 2 + y * Config.partSize)
-    ctx.moveTo(lineWidth / 2 + x * Config.partSize, y * Config.partSize)
-    ctx.lineTo(lineWidth / 2 + x * Config.partSize, y * Config.partSize + Config.partSize)
+    this.drawLine(leftX, topY + lineOffset, rightX, topY + lineOffset, ctx)
+    this.drawLine(leftX + lineOffset, topY, leftX + lineOffset, bottomY, ctx)
     ctx.stroke()
+  }
+
+  drawLine(x: number, y: number, toX: number, toY: number, ctx: CanvasRenderingContext2D) {
+    ctx.moveTo(x, y)
+    ctx.lineTo(toX, toY)
   }
 }
